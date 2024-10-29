401f19f7b5cced052fdf8216a1e261dafcee511b
Kirk Ouimet <kirk@kirkouimet.com>
2024-10-29 14:47:42 -0600

    Update title on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+// Dependencies - Next.js
+import { Metadata } from 'next';
+
+// Next.js Metadata
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: 'Connected • Network Monitoring and Security',
+    };
+}
+
 // Page - Wait List
 export { default } from '@structure/source/modules/wait-list/WaitListPage';
 
