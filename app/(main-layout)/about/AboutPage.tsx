d464807dd970ec93a8cbab7548e12d3bd324e0e2
kirkouimet <kirk@kirkouimet.com>
2025-05-24 08:00:05 -0600

    Updating about page

--- app/(main-layout)/about/AboutPage.tsx
+++ app/(main-layout)/about/AboutPage.tsx
@@ -17,39 +17,37 @@ export async function generateMetadata(): Promise<Metadata> {
 export function AboutPage() {
     // Render the component
     return (
-        <div className="container max-w-screen-md pt-8">
+        <div className="container pt-12">
             <h1>About Connected</h1>
 
             <hr className="my-6" />
 
             <Atlas particlesEnabled={false} scale={1.0} className="mx-auto my-16 w-32" />
 
-            <hr className="my-6" />
+            {/* <hr className="my-6" /> */}
 
-            <h4 className="mb-3">Mission and Vision</h4>
-            <div className="text-foreground-secondary">
-                <p>
-                    Our mission is to provide foundational network and Internet services that enhance connectivity and
-                    improve the digital experience for users worldwide.
-                </p>
-            </div>
+            <div className="mx-auto max-w-screen-sm">
+                <h4 className="mb-3">Mission and Vision</h4>
+                <div className="text-foreground-secondary">
+                    <p>
+                        Our mission is to provide foundational network and Internet services that enhance connectivity
+                        and improve the digital experience for users worldwide.
+                    </p>
+                </div>
 
-            <h4 className="mb-3 mt-6">Our Story</h4>
-            <div className="text-foreground-secondary">
-                <p>
-                    Connected was founded by Kirk Ouimet and Kameron Sheffield, who both have passion for technology.
-                    The journey began over 15 years ago with Kirk&apos;s creation of{' '}
-                    <Link href="https://www.yougetsignal.com/" target="_blank" className="link-blue">
-                        YouGetSignal
-                    </Link>
-                    , a popular network tool website. Building on the success and experience gained from YouGetSignal,
-                    Kirk and Kam envisioned a more advanced and comprehensive solution, leading to the development of
-                    connected.app.
-                </p>
-                <p className="mt-2">
-                    Connected is designed to be the successor to YouGetSignal, incorporating advanced features such as
-                    port checking, port monitoring, uptime tracking, and various network management tools.
-                </p>
+                <h4 className="mb-3 mt-6">Our Story</h4>
+                <div className="text-foreground-secondary">
+                    <p>
+                        Connected was founded by Kirk Ouimet and Kameron Sheffield, who both have passion for
+                        technology. The journey began over 15 years ago with Kirk&apos;s creation of{' '}
+                        <Link href="https://www.yougetsignal.com/" target="_blank" className="link-blue">
+                            YouGetSignal
+                        </Link>
+                        , a popular network tools website. Building on the success and experience gained from
+                        YouGetSignal, Kirk and Kam envisioned a more advanced and comprehensive solution, leading to the
+                        development of connected.app.
+                    </p>
+                </div>
             </div>
         </div>
     );
