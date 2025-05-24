61fd712537efa958f087d952101c42d111b48b73
kirkouimet <kirk@kirkouimet.com>
2025-05-24 09:01:27 -0600

    Updating open next and some lint commands

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,22 +1,21 @@
 // Dependencies - OpenNext
 import type { OpenNextConfig as OpenNextConfigurationInterface } from '@opennextjs/aws/types/open-next.js';
-import OpenNextCloudflareKvCache from '@opennextjs/cloudflare/kvCache';
 
 // OpenNextConfiguration
-// https://opennext.js.org/aws/config
-// Although this is a Cloudflare adapter, the @opennextjs/cloudflare package is now closely tied to the @opennextjs/aws package.
-// So, the configuration file documentation is found in the @opennextjs/aws package. (https://opennext.js.org/cloudflare/migrate-from-0.2)
+// https://opennext.js.org/
 const OpenNextConfiguration: OpenNextConfigurationInterface = {
     default: {
         override: {
             wrapper: 'cloudflare-node',
             converter: 'edge',
-            // Set `incrementalCache` to "dummy" to disable KV cache
-            incrementalCache: async () => OpenNextCloudflareKvCache,
+            proxyExternalRequest: 'fetch',
+            incrementalCache: 'dummy',
             tagCache: 'dummy',
             queue: 'dummy',
         },
     },
+
+    edgeExternals: ['node:crypto'],
 
     // Necessary to keep our 'build' command as 'opennextjs-cloudflare'
     buildCommand: 'next build', // DEFAULT: '{packageManager} run build',
@@ -27,6 +26,9 @@ const OpenNextConfiguration: OpenNextConfigurationInterface = {
             wrapper: 'cloudflare-edge',
             converter: 'edge',
             proxyExternalRequest: 'fetch',
+            incrementalCache: 'dummy',
+            tagCache: 'dummy',
+            queue: 'dummy',
         },
     },
 };
